bd4417fded4154ff1c327c56833593fbd084d41a
VladDrag <vdragomir@protonmail.com>
2022-07-15 20:23:33 +0200

    added dependencies for Entity Framework and generated boilerplate code for the Db API

---
+++ Fabryca_database_api/Controllers/FabrykaDbController.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Fabryca_database_api.Models;
+
+namespace Fabryca_database_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FabrykaDbController : ControllerBase
+    {
+        private readonly FabrycaContext _context;
+
+        public FabrykaDbController(FabrycaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/FabrukaDb
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Ticket>>> GetTicket()
+        {
+          if (_context.Ticket == null)
+          {
+              return NotFound();
+          }
+            return await _context.Ticket.ToListAsync();
+        }
+
+        // GET: api/FabrukaDb/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Ticket>> GetTicket(int id)
+        {
+          if (_context.Ticket == null)
+          {
+              return NotFound();
+          }
+            var ticket = await _context.Ticket.FindAsync(id);
+
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return ticket;
+        }
+
+        // PUT: api/FabrukaDb/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTicket(int id, Ticket ticket)
+        {
+            if (id != ticket.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(ticket).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TicketExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/FabrykaDb
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Ticket>> PostTicket(Ticket ticket)
+        {
+          if (_context.Ticket == null)
+          {
+              return Problem("Entity set 'FabrycaContext.Ticket'  is null.");
+          }
+            _context.Ticket.Add(ticket);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTicket", new { id = ticket.Id }, ticket);
+        }
+
+        // DELETE: api/FabrukaDb/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTicket(int id)
+        {
+            if (_context.Ticket == null)
+            {
+                return NotFound();
+            }
+            var ticket = await _context.Ticket.FindAsync(id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            _context.Ticket.Remove(ticket);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TicketExists(int id)
+        {
+            return (_context.Ticket?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Fabryca_database_api/Data/FabrycaContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Fabryca_database_api.Models;
+
+    public class FabrycaContext : DbContext
+    {
+        public FabrycaContext (DbContextOptions<FabrycaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Fabryca_database_api.Models.Ticket> Ticket { get; set; } = default!;
+    }
