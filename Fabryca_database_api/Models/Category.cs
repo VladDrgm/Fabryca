283f697575c2602d4da039e32d430bea0cccd3ee
VladDrag <vdragomir@protonmail.com>
2022-07-15 19:15:10 +0200

    added base models for the MVP. Fixes #16

---
+++ Fabryca_database_api/Models/Category.cs
@@ -0,0 +1,8 @@
+namespace Fabryca_database_api.Models;
+
+class Category 
+{
+  [Key]
+  public int Id { get; set; }
+  public string Name { get; set; }
+}
---
+++ Fabryca_database_api/Models/Ticket.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fabryca_database_api.Models;
+
+[Table("Tickets")]
+public class Ticket
+{
+  [Key]
+  public int Id { get; set; }
+  public string Title { get; set; }
+  public string Status { get; set; }
+
+  [DataType(DataType.Date)]
+  public DateTime CreatedAt { get; set; }
+
+  [StringLength(1000000)]
+  public string Description { get; set; }
+  [ForeignKey("Category")]
+  public int CategoryId { get; set; }
+  public Category Category { get; set; }
+}
