d339a0dd0c7d6d4a8aa9e015a218a3ece934438a
eirik lysenstoen <elysenstoen@gmail.com>
2022-07-18 15:55:01 +0200

    815 added CORS rules and GET and display for tickets. Fixes #31

--- fabryca_frontend/src/App.js
+++ fabryca_frontend/src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'; 
+import {useState, useEffect } from 'react';
 import { Button } from '@react95/core'
+import TicketCard from './components/TicketCard';
+
+
 
 function App() {
+  const [ticketList, setTicketList] = useState([]);
+
+  const getData = async () => {
+    const response = await fetch('https://localhost:7076/api/FabrykaDb/');
+    const data = await response.json();
+    console.log("Fetching data: ", data)
+    setTicketList(data)
+  }
+  
+  useEffect(() => {
+    getData()
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {ticketList.map(ticket =>
+            ( <TicketCard ticket={ticket} ticketList={ticketList} setTicketList={setTicketList} key={ticket.createdAt}/> ))}    
     </div>
   );
 }
