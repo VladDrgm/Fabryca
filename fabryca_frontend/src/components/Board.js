79cafe69299c1c5ac8f2c0117509c0ace835080c
eirik lysenstoen <elysenstoen@gmail.com>
2022-07-20 10:28:18 +0200

    815 added styling rules for board and child components

--- fabryca_frontend/src/components/Board.js
+++ fabryca_frontend/src/components/Board.js
@@ -2,54 +2,16 @@ import React from 'react';
 import {useState, useEffect } from 'react';
 import './Board.css'
 import { Frame } from '@react95/core'
+import CategoryCard from './CategoryCard';
 
 
 const Board = () => {
   return(
     
     <div className='board'>
-
-      <Frame padding={4} className='planned category'>
-        <Frame boxShadow="in" className='category__inner'>
-        <header className='category__header'> <h3>Planned</h3> </header>
-        <section className='category__list'>
-        <article className='ticket'>
-          <p>ticket1</p>
-        </article>
-        <article className='ticket'>
-        <p>ticket2</p>
-        </article>
-        </section>
-        </Frame>
-      </Frame>
-      
-      <Frame padding={4} className='planned category'>
-        <Frame boxShadow="in" className='category__inner'>
-        <header className='category__header'> <h3>Ongoing</h3> </header>
-        <section className='category__list'>
-        <article className='ticket'>
-          <p>ticket1</p>
-        </article>
-        <article className='ticket'>
-        <p>ticket2</p>
-        </article>
-        </section>
-        </Frame>
-      </Frame>
-
-      <Frame padding={4} className='planned category'>
-        <Frame boxShadow="in" className='category__inner'>
-        <header className='category__header'> <h3>Completed</h3> </header>
-        <section className='category__list'>
-        <article className='ticket'>
-          <p>ticket1</p>
-        </article>
-        <article className='ticket'>
-        <p>ticket2</p>
-        </article>
-        </section>
-        </Frame>
-      </Frame>
+      <CategoryCard category={'planned'}/>
+      <CategoryCard category={'ongoing'}/>
+      <CategoryCard category={'completed'}/>
     </div>
   )
 }
