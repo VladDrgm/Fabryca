8e808ff6c5fd3cf09b09599695b02686787572dd
Eirik <60854683+Eirikslys@users.noreply.github.com>
2022-07-20 16:46:09 +0200

    Added put buttons to change the category for buttons. Fixes #35 (#89)

--- fabryca_frontend/src/components/TicketCard.js
+++ fabryca_frontend/src/components/TicketCard.js
@@ -17,12 +17,59 @@ const TicketCard = ({ticket}) => {
   await response()
   console.log(response.ok)
   return response.ok
+  };
 
+  const makeOngoing = () => {
+    const url = 'https://localhost:7076/api/Tickets/' + ticket.title + '/category?categoryName=Ongoing'
+    
+    fetch(url,{
+      method: 'PUT',
+      mode: 'cors',
+      headers:{'Content-type':'application/json'}
+          }
+    )
+  };
+
+  const makePlanned = () => {
+    const url = 'https://localhost:7076/api/Tickets/' + ticket.title + '/category?categoryName=Planned'
+    
+    fetch(url,{
+      method: 'PUT',
+      mode: 'cors',
+      headers:{'Content-type':'application/json'}
+      }
+    )
+  };
+
+  const makeCompleted = () => {
+    const url = 'https://localhost:7076/api/Tickets/' + ticket.title + '/category?categoryName=Completed'
+    
+    fetch(url,{
+      method: 'PUT',
+      mode: 'cors',
+      headers:{'Content-type':'application/json'}
+      }
+    )
   };
 
   const handleDelete = (e) => {
     e.preventDefault();
     deleteToDatabase();
+  }
+
+  const handleMakeOngoing = (e) => {
+    e.preventDefault();
+    makeOngoing();
+  }
+
+  const handleMakePlanned = (e) => {
+    e.preventDefault();
+    makePlanned();
+  }
+
+  const handleMakeCompleted = (e) => {
+    e.preventDefault();
+    makeCompleted();
   }
   
   return(
@@ -35,6 +82,14 @@ const TicketCard = ({ticket}) => {
     <p>{ticket.description}</p>
     <p>{ticket.createdAt}</p>
     <Button onClick={handleDelete}>Delete</Button>
+    {ticket.categoryName === 'Planned' ? <Button onClick={handleMakeOngoing}>Ongoing</Button> : null}
+    {ticket.categoryName === 'Ongoing' ? 
+      <div>
+        <Button onClick={handleMakePlanned}>Planned</Button>
+        <Button onClick={handleMakeCompleted}>Completed</Button> 
+      </div>
+    : null} 
+    {ticket.categoryName === 'Completed' ? <Button onClick={handleMakeOngoing}>Ongoing</Button> : null}
     </section>
   </article>
   )
