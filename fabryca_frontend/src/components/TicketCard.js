9446b6e583054ee6fe94981f08a99a972918a3c5
mekkmann <99691324+mekkmann@users.noreply.github.com>
2022-07-21 12:57:58 +0200

    017-Add_initial_styling_to_ticketcard (#97)
    * 017-Add_initial_styling_to_ticketcard
    
    * Update TicketCard.js
    
    Co-authored-by: Eirik <60854683+Eirikslys@users.noreply.github.com>

--- fabryca_frontend/src/components/TicketCard.js
+++ fabryca_frontend/src/components/TicketCard.js
@@ -73,24 +73,31 @@ const TicketCard = ({ticket}) => {
   }
   
   return(
-    <article className='ticket'>
+    <article className='ticket__card'>
+
       <header className='ticket__header'>
-      <h3>{ticket.title}</h3>
-      <div> </div>
-    </header>
-    <section className='ticket__body'>
-    <p>{ticket.description}</p>
-    <p>{ticket.createdAt}</p>
-    <Button onClick={handleDelete}>Delete</Button>
-    {ticket.categoryName === 'Planned' ? <Button onClick={handleMakeOngoing}>Ongoing</Button> : null}
-    {ticket.categoryName === 'Ongoing' ? 
-      <div>
-        <Button onClick={handleMakePlanned}>Planned</Button>
-        <Button onClick={handleMakeCompleted}>Completed</Button> 
-      </div>
-    : null} 
-    {ticket.categoryName === 'Completed' ? <Button onClick={handleMakeOngoing}>Ongoing</Button> : null}
-    </section>
+        <h4>Title:</h4>
+        <h3>{ticket.title}</h3>
+      </header>
+
+      <section className='ticket__body'>
+        <h4>Description:</h4>
+        <p className='ticket__description'>{ticket.description}</p>
+        <h4>Created At:</h4>
+        <p className='ticket__date'>{ticket.createdAt}</p>
+        <div className='ticket__buttons'>
+          <Button onClick={handleDelete}>Delete</Button>
+          {ticket.categoryName === 'Planned' ? <Button onClick={handleMakeOngoing}>Ongoing</Button> : null}
+          {ticket.categoryName === 'Ongoing' ? 
+            <div>
+              <Button onClick={handleMakePlanned}>Planned</Button>
+              <Button onClick={handleMakeCompleted}>Completed</Button> 
+            </div>
+          : null} 
+          {ticket.categoryName === 'Completed' ? <Button onClick={handleMakeOngoing}>Ongoing</Button> : null}
+        </div>
+      </section>
+
   </article>
   )
 }
