9dfcb3e6ff42bc560f26a2caac6ce549047d4ebd
Pontus Liljekvist <alex.p.liljekvist@gmail.com>
2022-07-21 12:52:44 +0200

    017-Add_initial_styling_to_ticketcard

--- fabryca_frontend/src/components/TicketCard.js
+++ fabryca_frontend/src/components/TicketCard.js
@@ -3,7 +3,7 @@ import './TicketCard.css';
 import './Board.css';
 import { Button } from '@react95/core'
 
-const TicketCard = ({ticket}) => {
+const TicketCard = ({ticket, update, setUpdate}) => {
 
   const deleteToDatabase = async () => {
     const url = 'https://localhost:7076/api/Tickets/' + ticket.title
@@ -27,7 +27,7 @@ const TicketCard = ({ticket}) => {
       mode: 'cors',
       headers:{'Content-type':'application/json'}
           }
-    )
+    ).then(() => setUpdate(!update))
   };
 
   const makePlanned = () => {
@@ -38,7 +38,7 @@ const TicketCard = ({ticket}) => {
       mode: 'cors',
       headers:{'Content-type':'application/json'}
       }
-    )
+    ).then(() => setUpdate(!update))
   };
 
   const makeCompleted = () => {
@@ -49,7 +49,7 @@ const TicketCard = ({ticket}) => {
       mode: 'cors',
       headers:{'Content-type':'application/json'}
       }
-    )
+    ).then(() => setUpdate(!update))
   };
 
   const handleDelete = (e) => {
@@ -73,24 +73,31 @@ const TicketCard = ({ticket}) => {
   }
   
   return(
-    <article className='ticket'>
+    <article className='ticket__card'>
+
       <header className='ticket__header'>
-      <h3>{ticket.title}</h3>
-      <div> </div>
-    </header>
-    <section className='ticket__body'>
-    <p>{ticket.description}</p>
-    <p>{ticket.createdAt}</p>
-    <Button onClick={handleDelete}>Delete</Button>
-    {ticket.categoryName === 'Planned' ? <Button onClick={handleMakeOngoing}>Ongoing</Button> : null}
-    {ticket.categoryName === 'Ongoing' ? 
-      <div>
-        <Button onClick={handleMakePlanned}>Planned</Button>
-        <Button onClick={handleMakeCompleted}>Completed</Button> 
-      </div>
-    : null} 
-    {ticket.categoryName === 'Completed' ? <Button onClick={handleMakeOngoing}>Ongoing</Button> : null}
-    </section>
+        <h4>Title:</h4>
+        <h3>{ticket.title}</h3>
+      </header>
+
+      <section className='ticket__body'>
+        <h4>Description:</h4>
+        <p className='ticket__description'>{ticket.description}</p>
+        <h4>Created At:</h4>
+        <p className='ticket__date'>{ticket.createdAt}</p>
+        <div className='ticket__buttons'>
+          <Button onClick={handleDelete}>Delete</Button>
+          {ticket.categoryName === 'Planned' ? <Button onClick={handleMakeOngoing}>Ongoing</Button> : null}
+          {ticket.categoryName === 'Ongoing' ? 
+            <div>
+              <Button onClick={handleMakePlanned}>Planned</Button>
+              <Button onClick={handleMakeCompleted}>Completed</Button> 
+            </div>
+          : null} 
+          {ticket.categoryName === 'Completed' ? <Button onClick={handleMakeOngoing}>Ongoing</Button> : null}
+        </div>
+      </section>
+
   </article>
   )
 }
