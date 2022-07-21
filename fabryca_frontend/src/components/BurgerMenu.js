813cd8fa9ab1ba88f4204da5eb862119d0d96f08
Pontus Liljekvist <alex.p.liljekvist@gmail.com>
2022-07-21 12:09:42 +0200

    Fixed homelink in burgermenu. Fixes #91

--- fabryca_frontend/src/components/BurgerMenu.js
+++ fabryca_frontend/src/components/BurgerMenu.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 export default props => {
     return (
         <Menu>
-           <a className="menu-item"><Link to="">Home</Link></a>
+           <a className="menu-item"><Link to="/home">Home</Link></a>
            <a className="menu-item"><Link to="/faq">FAQ</Link></a>
            <a className="menu-item"><Link to="/privacy">Privacy</Link></a>
            <a className="menu-item"><Link to="/new">+ Ticket</Link></a>
