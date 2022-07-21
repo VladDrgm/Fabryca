6b9c50ec7df97efba5698304775ba5c35596db94
Pontus Liljekvist <alex.p.liljekvist@gmail.com>
2022-07-21 16:07:22 +0200

    Removed PHAddTicket and added a real button in boardpage

--- fabryca_frontend/src/components/BoardPage.js
+++ fabryca_frontend/src/components/BoardPage.js
@@ -1,39 +1,21 @@
 // import logo from './logo.svg';
 import '../App.css';
 import React from 'react'; 
-import { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 import { Button, ThemeProvider } from '@react95/core'
-import TicketCard from './TicketCard';
-import NewTicketPage from './NewTicketPage';
+import { Link } from 'react-router-dom';
 import Board from './Board';
-import { BrowserRouter as Router, Routes, Route, Redirect} from 'react-router-dom';
 
 
 
 function BoardPage() {
-//   const [ticketList, setTicketList] = useState([]);
-
-//   const getData = async () => {
-//     const response = await fetch('https://localhost:7076/api/Tickets/');
-//     const data = await response.json();
-//     console.log("Fetching data: ", data)
-//     setTicketList(data)
-//   }
-  
-//   useEffect(() => {
-//     getData()
-//   }, []);
-  // <TicketForm ticketList={ticketList} setTicketList={setTicketList}/>
-  // {ticketList.map(ticket =>
-  //   ( <TicketCard ticket={ticket} ticketList={ticketList} setTicketList={setTicketList} key={ticket.createdAt}/> ))}    
-
 
   return (
     <ThemeProvider>
     <div className="App" id="outer-container">
       <div id="page-wrap">
       <Navbar></Navbar>
+      <Button className="navbar__button"><Link to="/new">Add a Ticket</Link></Button>
       <Board></Board>
       </div>
     </div>
