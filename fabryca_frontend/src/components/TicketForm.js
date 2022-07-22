bc2d1aae300cbd765096591218d9ff357bd6e68e
eirik lysenstoen <elysenstoen@gmail.com>
2022-07-22 11:29:42 +0200

    Changed buttons and text inputs to the 95 style. Fixes 115

--- fabryca_frontend/src/components/TicketForm.js
+++ fabryca_frontend/src/components/TicketForm.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { Frame } from '@react95/core'
+import { Frame, Input, Button} from '@react95/core'
 import './TicketForm.css';
 import { useNavigate } from 'react-router-dom';
 
@@ -47,10 +47,10 @@ import { useNavigate } from 'react-router-dom';
       <Frame className='ticket__form__frame'>
         <form onSubmit={handleSubmit} className="ticket__form">
           <h3 className='ticket__form__title'>Add a New Ticket</h3>
-          <input type='text' placeholder='Title' value={title} className={'ticket__form__field'} onChange={e => setTitle(e.target.value)}/>
-          <input type='text'placeholder='Status' value={status} className={'ticket__form__field'} onChange={e => setStatus(e.target.value)}/>
-          <input type='text'placeholder='Description' value={description} className={'ticket__form__field'} onChange={e => setDescription(e.target.value)}/>
-          <button type='submit'> Add a new ticket </button> 
+          <Input type='text' placeholder='Title' value={title} className={'ticket__form__field'} onChange={e => setTitle(e.target.value)}/>
+          <Input type='text'placeholder='Status' value={status} className={'ticket__form__field'} onChange={e => setStatus(e.target.value)}/>
+          <Input type='text'placeholder='Description' value={description} className={'ticket__form__field'} onChange={e => setDescription(e.target.value)}/>
+          <Button type='submit'> Add a new ticket </Button> 
         </form>
       </Frame>
     )
