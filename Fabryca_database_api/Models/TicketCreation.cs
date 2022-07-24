7069116d4a4f615cf505e7fa0bffb0591a89c2fa
VladDrag <74666581+VladDrag@users.noreply.github.com>
2022-07-24 18:58:38 +0200

    Addeduser strings. Fixes #126 (#128)

--- Fabryca_database_api/Models/TicketCreation.cs
+++ Fabryca_database_api/Models/TicketCreation.cs
@@ -7,10 +7,16 @@ public class TicketCreation
     Title = ticket.Title;
     Status = ticket.Status;
     Description = ticket.Description;
+    CreatedBy = ticket.CreatedBy;
+    AssignedTo = ticket.AssignedTo;
   }
   public TicketCreation() {}
 
   public string Title { get; set; }
   public string Status { get; set; }
   public string Description { get; set; }
+
+  public string CreatedBy { get; set; }
+
+  public string AssignedTo { get; set; }
 }
