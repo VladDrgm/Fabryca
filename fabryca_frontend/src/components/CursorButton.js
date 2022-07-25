594169ad2296c0c07dc63bb5b189eaef3d6c4fa9
Eirik <60854683+Eirikslys@users.noreply.github.com>
2022-07-25 12:19:45 +0200

    Modified all buttons to be CursorButton components. Added Onclick prop to Cursorbutton. Fixes #115 (#137)
    Co-authored-by: VladDrag <74666581+VladDrag@users.noreply.github.com>

--- fabryca_frontend/src/components/CursorButton.js
+++ fabryca_frontend/src/components/CursorButton.js
@@ -2,14 +2,18 @@ import styled from 'styled-components';
 import { Button, Cursor } from '@react95/core';
 
 
-const CursorButton = ({type, text}) => {
+const CursorButton = ({type, text, onClick}) => {
 
   const CursorButtonElement = styled(Button)`
   ${({ type }) => Cursor[type]};
   `;
+  if (onClick){
   return (
-    <CursorButtonElement type={type} > {text} </CursorButtonElement> 
-  )
+    <CursorButtonElement type={type} onClick={onClick} > {text} </CursorButtonElement> 
+  )} else {
+    return(
+    <CursorButtonElement type={type} > {text} </CursorButtonElement>)
+  }
 }
 
 export default CursorButton;
