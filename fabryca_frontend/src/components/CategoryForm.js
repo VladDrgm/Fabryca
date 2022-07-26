7ec2c25af8380091d03ca9a5ccd1e9d4d9e3801b
Pontus Liljekvist <alex.p.liljekvist@gmail.com>
2022-07-26 23:44:23 +0200

    Fixed another blank-out issue

--- fabryca_frontend/src/components/CategoryForm.js
+++ fabryca_frontend/src/components/CategoryForm.js
@@ -18,26 +18,26 @@ import CursorButton from './CursorButton';
       }).then(r=>r.json()).then(res=>{
         if(res){
           console.log(res);
+          setName('');
+          navigate(-1);
         }
       });
-      localStorage.clear();
       
     }
-
+    
     const navigate = useNavigate();
     const handleSubmit = async (e) => {
       e.preventDefault();
       
       postToDatabase();
       
-      setName('');
-      navigate(-1);
     }
     
     return(
       <Frame className='ticket__form__frame'>
         <form onSubmit={handleSubmit} className="ticket__form">
           <h3 className='ticket__form__title'>Add a New Category to: {projName}</h3>
+          <h4>Title:</h4>
           <Input type='text' placeholder='Title' value={name} className={'ticket__form__field'} onChange={e => setName(e.target.value)}/>
           <CursorButton type={'Pointer'} text={'Add Category'} />
         </form>
