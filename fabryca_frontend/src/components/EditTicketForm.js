ea3a7f54b7298b3d10880d8098deeaadfa863cab
Eirik <60854683+Eirikslys@users.noreply.github.com>
2022-07-27 18:05:32 +0200

    Fixed styling for the edit form (#222)

--- fabryca_frontend/src/components/EditTicketForm.js
+++ fabryca_frontend/src/components/EditTicketForm.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Frame, Input } from '@react95/core';
 import './TicketForm.css';
 import { useNavigate } from 'react-router-dom';
 import CursorButton from './CursorButton';
 
 
- const EditTicketForm = () =>{
+const EditTicketForm = () =>{
     const [description, setDescription] = useState('');
     const [title, setTitle] = useState('');
     const [status, setStatus] = useState('');
     const [category, setCategory] = useState('');
     const oldTicketName = localStorage.getItem('name');
     const projName = localStorage.getItem('projectName');
+    const [oldTicket, setOldTicket] = useState();
   
     const putToDatabase = async () => {
-
       await fetch(`https://fabrycaapi.azurewebsites.net/api/Tickets/${oldTicketName}/${projName}/ticket?newTitle=${title}&newStatus=${status}&newCategoryName=${category}&newDescription=${description}`,{
         method: 'PUT',
         mode: 'cors',
@@ -25,8 +25,21 @@ import CursorButton from './CursorButton';
         }
       });
       localStorage.clear();
-  
     }
+
+    const getTicket = async () => {
+      const response = await fetch(`https://fabrycaapi.azurewebsites.net/api/Tickets/${oldTicketName}`);
+      const data = await response.json();
+      setOldTicket(data);
+
+    }
+
+    useEffect(() => {
+      getTicket();
+      console.log("getting ticket")
+    }, []);
+
+    
     const navigate = useNavigate();
     const handleSubmit = async (e) => {
       e.preventDefault();
@@ -44,10 +57,14 @@ import CursorButton from './CursorButton';
       <Frame className='ticket__form__frame'>
         <form onSubmit={handleSubmit} className="ticket__form">
           <h3 className='ticket__form__title'>Edit Ticket: {oldTicketName}</h3>
-          <Input type='text' placeholder='Title' value={title} className={'ticket__form__field'} onChange={e => setTitle(e.target.value)}/>
-          <Input type='text' placeholder='Status' value={status} className={'ticket__form__field'} onChange={e => setStatus(e.target.value)}/>
-          <Input type='text' placeholder='Description' value={description} className={'ticket__form__field'} onChange={e => setDescription(e.target.value)}/>
-          <Input type='text' placeholder='Category' value={category} className={'ticket__form__field'} onChange={e => setCategory(e.target.value)}/>
+          <label style={{textAlign: 'left', width: '90%'}}>title</label>
+          <Input type='text' placeholder={oldTicket && oldTicket.title ? oldTicket.title : 'Title'} value={title} className={'ticket__form__field'} onChange={e => setTitle(e.target.value)}/>
+          <label style={{textAlign: 'left', width: '90%'}}>status</label>
+          <Input type='text' placeholder={oldTicket && oldTicket.status ? oldTicket.status : 'Status'} value={status} className={'ticket__form__field'} onChange={e => setStatus(e.target.value)}/>
+          <label style={{textAlign: 'left', width: '90%'}}>description</label>
+          <Input type='text' placeholder={oldTicket && oldTicket.description ? oldTicket.description : 'Description'} value={description} className={'ticket__form__field'} onChange={e => setDescription(e.target.value)}/>
+          <label style={{textAlign: 'left', width: '90%'}}>category</label>
+          <Input type='text' placeholder={oldTicket && oldTicket.categoryName ? oldTicket.categoryName : 'Category'} value={category} className={'ticket__form__field'} onChange={e => setCategory(e.target.value)}/>
           <CursorButton type={'Pointer'} text={'Update ticket'} />
         </form>
       </Frame>
