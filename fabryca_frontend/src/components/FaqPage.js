424c23a110966adb99318d1d4b4a5e0ad499539f
VladDrag <74666581+VladDrag@users.noreply.github.com>
2022-07-28 10:49:23 +0200

    V1.3 (#227)
    * Attempting to fix blanking out on refresh (#190)
    
    * Fixed some refresh issues (#192)
    
    * Updated GetCategory in the api to have a functional return statement and fixed some more refresh issues (#193)
    
    * Fixed another blank-out issue (#194)
    
    * Fixed refresh issue (#195)
    
    * Added prod url (#197)
    
    * On project creation, you get a Planned category automatically created. Fixes #199 (#200)
    
    * Fixed ticketform (#201)
    
    * Removed unused var (#203)
    
    * 017 removed an h (#205)
    
    * Removed unused var
    
    * Removed an H
    
    * 017 landing page styling (#207)
    
    * Removed unused var
    
    * Removed an H
    
    * Add styling LandingPage
    
    * Fixed an import (#208)
    
    * Added styling and updates (#209)
    
    * added number of tickets per category
    
    * fixed number of tickets per category
    
    * quick update on number of tickets per category (#212) (#213)
    
    * Attempting to fix blanking out on refresh (#190)
    
    * Fixed some refresh issues (#192)
    
    * Updated GetCategory in the api to have a functional return statement and fixed some more refresh issues (#193)
    
    * Fixed another blank-out issue (#194)
    
    * Fixed refresh issue (#195)
    
    * Added prod url (#197)
    
    * On project creation, you get a Planned category automatically created. Fixes #199 (#200)
    
    * Fixed ticketform (#201)
    
    * Removed unused var (#203)
    
    * 017 removed an h (#205)
    
    * Removed unused var
    
    * Removed an H
    
    * 017 landing page styling (#207)
    
    * Removed unused var
    
    * Removed an H
    
    * Add styling LandingPage
    
    * Fixed an import (#208)
    
    * Added styling and updates (#209)
    
    * added number of tickets per category
    
    * fixed number of tickets per category
    
    Co-authored-by: mekkmann <99691324+mekkmann@users.noreply.github.com>
    
    Co-authored-by: mekkmann <99691324+mekkmann@users.noreply.github.com>
    
    * Implemented win95 style pointer cursor for all remaining buttons and clickable fields in app (#215)
    
    * Added styling and route (#216)
    
    * Added win95 font, increased button size and made categories unfurl by default (#217)
    
    * Added styling (#220)
    
    * Fixed Unfurl/Furl (#221)
    
    * Fixed styling for the edit form (#222)
    
    * Fixed error (#224)
    
    * fixed FAQ page
    
    Co-authored-by: mekkmann <99691324+mekkmann@users.noreply.github.com>
    Co-authored-by: Eirik <60854683+Eirikslys@users.noreply.github.com>

--- fabryca_frontend/src/components/FaqPage.js
+++ fabryca_frontend/src/components/FaqPage.js
@@ -12,6 +12,13 @@ function FaqPage() {
       <div id="page-wrap">
       <Navbar></Navbar>
       <h1>Faq page</h1>
+      <p>
+        1) Why does this website look like this?
+          A: We are 3 passionate developers with a feeling for nostalgia and vintage.
+             We really like the idea of basing our design on Windows95 / Windows98.
+             We were lucky and found a very good component library with React95; you can
+             check it out here: <a href="https://github.com/React95/React95">Click here</a>
+      </p>
       </div>
     </div>
     </ThemeProvider>
