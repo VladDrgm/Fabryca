9e410e956f9bb67752c40ebc6daa9af089b5dff9
Eirik <60854683+Eirikslys@users.noreply.github.com>
2022-08-05 14:09:48 +0200

    Made the category update buttons for tickets dynamically generated. Fixes #230 (#231)

--- fabryca_frontend/src/components/CategoryCard.js
+++ fabryca_frontend/src/components/CategoryCard.js
@@ -1,12 +1,12 @@
 import React from 'react'; 
 import {useState, useEffect } from 'react';
-import './Board.css'
+import './CategoryCard.css'
 import TicketCard from './TicketCard';
 import { Frame } from '@react95/core';
 import { Progman46, Progman43 } from '@react95/icons';
 import CursorButton from './CursorButton';
 
-const CategoryCard = ({category, ticketList, setTicketList}) => {
+const CategoryCard = ({category, ticketList, setTicketList, categoryList}) => {
 
   const [localTicketList, setLocalTicketList] = useState([]);
 
@@ -56,7 +56,7 @@ const CategoryCard = ({category, ticketList, setTicketList}) => {
     </header>
     <section className={showCategory === true ? 'category__list--show' : 'category__list--hidden'}>
     {localTicketList.length !== 0 ? localTicketList.map(ticket =>
-        ( <TicketCard setTicketList={setTicketList} ticketList={ticketList} ticket={ticket} key={ticket.createdAt + ticket.title}/> )) : <h4>There aren't any tickets for this category</h4>}
+        ( <TicketCard setTicketList={setTicketList} ticketList={ticketList} ticket={ticket} category={category} categoryList={categoryList} key={ticket.createdAt + ticket.title}/> )) : <h4>There aren't any tickets for this category</h4>}
     </section>
         {showCategory === true ? <CursorButton type={'Pointer'} text={'Delete Category'} onClick={deleteCategory} /> : null}
     </Frame>
